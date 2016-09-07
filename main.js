c85b65fa1c150d4700e56bde81e49868f9d00912
mingttong <zhouwunan@qq.com>
2016-09-07 18:55:51 +0800

    享元模式

--- main.js
+++ main.js
@@ -729,3 +729,83 @@ var Composite = function () {
     ).show();
 
 }();
+
+console.log('###########################################');
+console.log('享元模式');
+
+var FlyweightPattern = function () {
+
+    var article = ['这是第1条新闻', '这是第2条新闻', '这是第3条新闻', '这是第4条新闻', '这是第5条新闻',
+        '这是第6条新闻', '这是第7条新闻', '这是第8条新闻', '这是第9条新闻', '这是第10条新闻',
+        '这是第11条新闻', '这是第12条新闻', '这是第13条新闻', '这是第14条新闻', '这是第15条新闻', '这是第16条新闻'];
+
+    console.log("####享元对象");
+
+    var Flyweight = function () {
+        // 已创建的元素
+        var created = [];
+        // 创建一个新闻包装容器
+        function create() {
+            var dom = document.createElement('div');
+            // 将容器插入新闻列表容器中
+            document.getElementById('container').appendChild(dom);
+            // 缓存新创建的元素
+            created.push(dom);
+            // 返回创建的新元素
+            return dom;
+        }
+        return {
+            // 获取创建的新闻元素的方法
+            getDiv : function () {
+                // 如果已经创建的元素小于当前页面元素总个数，则创建
+                if (created.length < 5) {
+                    return create();
+                } else {
+                    // 获取第一个元素，并插入最后面
+                    var div = created.shift();
+                    created.push(div);
+                    return div;
+                }
+            }
+        };
+    }();
+
+    console.log('####实现需求');
+
+    var paper = 0,
+        num = 5,
+        len = article.length;
+
+    // 添加5条新闻
+    for (var i = 0; i < 5; i += 1) {
+        if (article[i]) {
+            // 通过享元类获取创建的元素并写入新闻内容
+            Flyweight.getDiv().innerHTML = article[i];
+        }
+    }
+
+    // 下一页按钮绑定事件
+    document.getElementById('next_page').onclick = function () {
+        // 如果新闻内容不足5条则返回
+        if (article.length < 5) {
+            return;
+        }
+
+        var n = ++paper * num % len, // 获取当前页的第一条新闻索引
+            j = 0;                   // 循环变量
+
+        // 插入5条新闻
+        for (; j < 5; j += 1) {
+            // 如果存在第 n + j 条则插入
+            if (article[n + j]) {
+                Flyweight.getDiv().innerHTML = article[n + j];
+            } else if (article[n + j - len]) {
+                // 否则插入起始位置第 n + j - len 条
+                Flyweight.getDiv().innerHTML = article[n + j - len];
+            } else {
+                Flyweight.getDiv().innerHTML = "";
+            }
+        }
+    };
+
+}();
